51ef2e3ec161c7c83cf0132ec42c05dffd067f3d
AlexGeniusMan <alexgeniusman@gmail.com>
2020-12-14 18:36:48 +0300

    min filling added

--- main_app/models.py
+++ main_app/models.py
@@ -18,7 +18,7 @@ class Artifact(models.Model):
     name = models.CharField(_("Название"), max_length=100)
     img = models.ImageField(_("Фотография"), null=True, upload_to='Artifact photos', blank=True)
     audio = models.FileField(_("Аудио"), upload_to='Artifact audios', blank=True)
-    description = models.TextField(_("Описание"), max_length=1000, blank=True)
+    description = models.TextField(_("Описание"), max_length=10000, blank=True)
 
     class Meta:
         verbose_name = 'Экспонат'
