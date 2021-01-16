76e080b6679f421330771a5332cbac3103b4d77a
AlexGeniusMan <alexgeniusman@gmail.com>
2021-01-16 19:14:18 +0300

    test added

--- main_app/tests.py
+++ main_app/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 class ViewsTestCase(TestCase):
     def test_index_loads_properly(self):
         """The index page loads properly"""
-        response = self.client.get('localhost:8000/api/m-admin')
+        response = self.client.get('http://127.0.0.1:8000/api/m-admin')
         self.assertEqual(response.status_code, 200)
