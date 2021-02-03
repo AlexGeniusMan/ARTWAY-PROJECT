be0e3e82f125053a89e1fba5227cc8475e9d6e83
AlexGeniusMan <alexgeniusman@gmail.com>
2021-02-03 21:19:09 +0300

    swagger added

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,23 @@ from django.conf.urls.static import static
 import main_app.views as views
 import main_app.views_visitor as views_visitor
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API documentation for ArtWay project",
+      default_version='v1',
+      # description="",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="dev.alexgeniusman@gmail.com"),
+      license=openapi.License(name="Apache License 2.0"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 # Админ-панель (используется только при режиме разработки)
 if settings.DEBUG:
@@ -104,6 +121,10 @@ if settings.DEBUG:
 
         # Вызвать метод save() у всех экспонатов (используется для переноса сервиса с IP/домена на IP/домен)
         # path('api/update_all_qrs', views.UpdateAllQRsView.as_view()),
+
+        url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
