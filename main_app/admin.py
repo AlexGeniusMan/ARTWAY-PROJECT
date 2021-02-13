5144b2dc4b444a1e5e9a69d5e3f00055aa25b93d
AlexGeniusMan <alexgeniusman@gmail.com>
2021-02-13 10:30:28 +0300

    dev admin panel fixed

--- main_app/admin.py
+++ main_app/admin.py
@@ -6,7 +6,6 @@ from django.contrib.auth.admin import UserAdmin
 
 class ArtifactAdmin(admin.ModelAdmin):
     search_fields = ('name',)
-    # fields = ('name', 'img', 'audio', 'description')
 
 
 class CustomUserAdmin(UserAdmin):
@@ -15,11 +14,8 @@ class CustomUserAdmin(UserAdmin):
         (_('Авторизация'), {'fields': ('username', 'email', 'password')}),
         (_('Основная информация'),
          {'fields': ('last_name', 'first_name', 'middle_name', 'museum')}),
-        # (_('Университет'), {'fields': ('university', 'faculty', 'specialty', 'course_type', 'course')}),
-        # (_('Ссылки'),
-        #  {'fields': ('link_vk', 'link_facebook', 'link_instagram', 'link_telegram', 'link_github', 'link_dribble')}),
         (_('Права доступа'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        # (_('Другое'), {'fields': ('last_login', 'date_joined', 'priority')}),
+        (_('Другое'), {'fields': ('last_login', 'date_joined', 'priority')}),
     )
     add_fieldsets = (
         (None, {
